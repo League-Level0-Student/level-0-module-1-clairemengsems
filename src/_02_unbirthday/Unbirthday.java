fd0fa442e181725752093b175f5f7b0a93f7fa06
Claire Meng <Meng.Claire@SEMS259.LAB>
2019-11-06 16:26:47 -0800

    Unbirthday, Remarkable

---
+++ src/_02_unbirthday/Unbirthday.java
@@ -0,0 +1,30 @@
+package _02_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+String birthday	= JOptionPane.showInputDialog("When is your Birthday? mm/dd");	
+
+
+
+
+if( birthday.equals("11/06")){
+	
+
+JOptionPane.showMessageDialog(null,"Hey, Happy Birthday!"); 
+		
+}		
+else {		
+		
+JOptionPane.showMessageDialog(null, "Oh, well happy Unbirthday!"); 		
+JOptionPane.showMessageDialog(null, "lol"); 			
+		
+		
+}			
+	}
+
+}
--- src/_03_remarkable/Remarkable.java
+++ src/_03_remarkable/Remarkable.java
@@ -8,10 +8,52 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-
+String andrew = "loud.";
+String eric = "intresting.....";
+String bob = "cool.";
+String anna = "nice.";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
 
+String input = JOptionPane.showInputDialog("Whats your name? (choose out of andrew, eric, bob, or anna.)");
+
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
+if (input.equals("andrew")){
+JOptionPane.showMessageDialog(null, "You are " + andrew);
+}
+
+if (input.equals("eric")){
+JOptionPane.showMessageDialog(null, "You are " + eric);
+}
+
+if (input.equals("bob")){
+JOptionPane.showMessageDialog(null, "You are " + bob);
+}
+
+if (input.equals("anna")){
+JOptionPane.showMessageDialog(null, "You are " + anna);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	}
 }
